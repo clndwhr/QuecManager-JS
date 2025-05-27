8eb98aea89c11536f39dfb52834acd6478356856
Christopher Landwehr <clndwhr@gmail.com>
2025-05-27 15:52:45 -0400

    get-capabilities cgi expanded

--- scripts/cgi-bin/quecmanager/get-capabilities.sh
+++ scripts/cgi-bin/quecmanager/get-capabilities.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
+PLATFORM=$(cat /sys/devices/soc0/machine)
 SPEEDTEST=false
-FIRMWARE_VERSION=$(sms_tool at -t 3 'AT+QGMR' | sed -n '2p' | tr -d '\r')
-MODEM_TYPE=$(sms_tool at -t 3 'AT+CGMM' | sed -n '2p' | tr -d '\r')
-MODEM_MANUFACTURER=$(sms_tool at -t 3 'AT+CGMI' | sed -n '2p' | tr -d '\r')
 
 if [ command -v speedtest >/dev/null 2>&1 ]; then
     SPEEDTEST=true
 fi
+
+case $PLATFORM in 
+    *LEMUR*)
+        SPEEDTEST=false
+        EXPERIMENTAL_QUECWATCH=false
+        EXPERIMENTAL_QUECPROFILES=false
+        EXPERIMENTAL_KEEPALIVE=false
+        FIRMWARE_VERSION=$(atcmd 'AT+QGMR' | sed -n '2p' | tr -d '\r'| sed 's/\x1B\[[0-9;:]*[mGKHF]//g')
+        MODEM_TYPE=$(atcmd 'AT+CGMM' | sed -n '2p' | tr -d '\r'| sed 's/\x1B\[[0-9;:]*[mGKHF]//g')
+        MODEM_MANUFACTURER=$(atcmd 'AT+CGMI' | sed -n '2p' | tr -d '\r'| sed 's/\x1B\[[0-9;:]*[mGKHF]//g')
+
+        ;;
+    *)
+        EXPERIMENTAL_QUECWATCH=true
+        EXPERIMENTAL_QUECPROFILES=true
+        EXPERIMENTAL_KEEPALIVE=true
+        FIRMWARE_VERSION=$(sms_tool at -t 3 'AT+QGMR' | sed -n '2p' | tr -d '\r')
+        MODEM_TYPE=$(sms_tool at -t 3 'AT+CGMM' | sed -n '2p' | tr -d '\r')
+        MODEM_MANUFACTURER=$(sms_tool at -t 3 'AT+CGMI' | sed -n '2p' | tr -d '\r')
+
+        ;;
+esac
+
 
 RESPONSE="Content-Type: application/json\n"
 RESPONSE="${RESPONSE}Cache-Control: no-cache, no-store, must-revalidate\n"
@@ -15,6 +36,9 @@ RESPONSE="${RESPONSE}Pragma: no-cache\n"
 RESPONSE="${RESPONSE}Expires: 0\n\n"
 RESPONSE="${RESPONSE}{\n"
 RESPONSE="${RESPONSE}  \"speedtest\": ${SPEEDTEST},\n"
+RESPONSE="${RESPONSE}  \"quecwatch\": ${EXPERIMENTAL_QUECWATCH},\n"
+RESPONSE="${RESPONSE}  \"quecprofiles\": ${EXPERIMENTAL_QUECPROFILES},\n"
+RESPONSE="${RESPONSE}  \"keepalive\": ${EXPERIMENTAL_KEEPALIVE},\n"
 RESPONSE="${RESPONSE}  \"firmware\": \"${FIRMWARE_VERSION}\",\n"
 RESPONSE="${RESPONSE}  \"modem\": \"${MODEM_TYPE}\",\n"
 RESPONSE="${RESPONSE}  \"manufacturer\": \"${MODEM_MANUFACTURER}\"\n"
